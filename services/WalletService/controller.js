ca310eb411c02eae6e7285db0e6361db0b4a9f98
mohamadmahdytamimipoor <mohamadmahdytamimipoor@dhcppc3>
2025-04-09 22:48:17 +0330

    v0.9.0

--- services/WalletService/controller.js
+++ services/WalletService/controller.js
@@ -1,3 +1,5 @@
+import {Wallet} from "../../models.js";
+
 export  const depositAccount = [
     depos
 ]
@@ -5,5 +7,29 @@ export  const depositAccount = [
 
 
 async function depos(req , res){
-  return
+
+    const user_id = req.body.user_id
+    const wallet = req.body.wallet
+
+
+    const w = await Wallet.findOne({where : {user_id: user_id}})
+    if (!w){
+        return res.status(404).json({
+            success : false
+        })
+    }
+
+    w.wallet = wallet
+
+    w.save()
+
+
+
+
+
+
+  return res.status(200).json({
+      success : true ,
+      data : w
+  })
 }
